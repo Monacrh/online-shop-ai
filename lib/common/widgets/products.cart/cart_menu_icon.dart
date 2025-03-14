d63d3a8cbede6d5dce5295d8011ee29cf603b199
Monarch <bryandhaniel123@gmail.com>
2025-03-15 04:36:33 +0700

    feat: add cart screen

--- lib/common/widgets/products.cart/cart_menu_icon.dart
+++ lib/common/widgets/products.cart/cart_menu_icon.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:online_shop_ai/features/shop/screens/cart/cart.dart';
 import 'package:online_shop_ai/utils/constants/colors.dart';
 
 import '../../../utils/helpers/helper_functions.dart';
@@ -8,17 +10,19 @@ class TCartCounterIcon extends StatelessWidget {
   const TCartCounterIcon({
     super.key,
     required this.onPressed, 
-    required this.iconColor,
+    this.iconColor,
+    this.counterBgColor,
+    this.counterTextColor,
   });
 
   final VoidCallback onPressed;
-  final Color? iconColor;
+  final Color? iconColor, counterBgColor, counterTextColor;
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        IconButton(onPressed: (){}, icon: Icon(Iconsax.shopping_bag, color: THelperFunctions.isDarkMode(context) ? TColors.white : TColors.black)),
+        IconButton(onPressed: () => Get.to(() => const CartScreen()), icon: Icon(Iconsax.shopping_bag, color: THelperFunctions.isDarkMode(context) ? TColors.white : TColors.black)),
         Positioned(
           right: 0,
           child: Container(
