4af44f96deb9ea71f3a5be9e26a6aab1338d803f
Monarch <bryandhaniel123@gmail.com>
2025-03-24 18:43:53 +0700

    initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-// import 'package:online_shop_ai/app.dart';
-// import 'package:online_shop_ai/utils/constants/colors.dart';
-// import 'package:online_shop_ai/utils/theme/theme.dart';
+// import 'package:get/get.dart';
 
 import 'app.dart';
+// import 'firebase_options.dart';
 
-void main() {
+
+Future<void> main() async {
 
   // Todo : Add Widgets Binding
   // Todo : Init Local Storage
   // Todo : Await Native Splash
   // Todo : Initialize Firebase
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then(
+  //   (FirebaseApp value) => Get.put(AuthenticationRepository())
+  // );
   // Todo : Initialize Authentication
   
   runApp(const App());
